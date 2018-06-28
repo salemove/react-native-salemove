7dc9de163a637578a388f57134b4ee39c0e19fd2
Dmitry Preobrazhenskiy <d.preobrahzenskiy@gmail.com>
2018-06-28 17:24:15 +0300

    Add additional events

--- ios/SaleMove/SaleMove.h
+++ ios/SaleMove/SaleMove.h
@@ -1,6 +1,16 @@
 #import <Foundation/Foundation.h>
 #import <React/RCTBridgeModule.h>
 #import <React/RCTEventEmitter.h>
+#import <SalemoveSDK/SalemoveSDK-Swift.h>
+
+typedef void (^VoidCompletitionBlock)(void);
+typedef void (^MessageCompletionBlock)(Message * message);
+
+@interface InteractableClient : NSObject <Interactable>
+@property (nonatomic, copy) VoidCompletitionBlock startEvent;
+@property (nonatomic, copy) VoidCompletitionBlock endEvent;
+@property (nonatomic, copy) MessageCompletionBlock operatorMessageEvent;
+@end
 
 @interface SaleMove : RCTEventEmitter <RCTBridgeModule>
 
--- ios/SaleMove/SaleMove.m
+++ ios/SaleMove/SaleMove.m
@@ -7,11 +7,61 @@
 //
 
 #import "SaleMove.h"
-#import <SalemoveSDK/SalemoveSDK-Swift.h>
 
 @implementation SaleMove
 
 RCT_EXPORT_MODULE(SaleMove);
+
+- (NSArray<NSString *> *)supportedEvents {
+    return @[
+             @"operator_message",
+             @"engagement_start",
+             @"engagement_end"
+             ];
+}
+
+RCT_EXPORT_METHOD(monitorOperatorMessages) {
+    // XXX
+    // The messages should be emitted even when this method is not called by
+    // the javascript counterpart. Current implementation is fragile because
+    // it leaves `operatorMessageEvent` uninitialized if this method is not
+    // called. And does not allow multiple calls.
+    dispatch_async(dispatch_get_main_queue(), ^{
+        AppDelegate *delegate = (AppDelegate *)[UIApplication sharedApplication].delegate;
+        InteractableClient *client = delegate.interactableClient;
+        
+        client.operatorMessageEvent = ^(Message * message){
+            [self sendEventWithName:@"operator_message" body:@{
+                                                               @"id": message.id,
+                                                               @"content": message.content,
+                                                               @"sender": @"operator"
+                                                               }];
+        };
+    });
+}
+
+RCT_EXPORT_METHOD(monitorEngagementStart) {
+    dispatch_async(dispatch_get_main_queue(), ^{
+        AppDelegate *delegate = (AppDelegate *)[UIApplication sharedApplication].delegate;
+        InteractableClient *client = delegate.interactableClient;
+        
+        client.startEvent = ^{
+            [self sendEventWithName:@"engagement_start" body:@{}];
+        };
+    });
+}
+
+RCT_EXPORT_METHOD(monitorEngagementEnd) {
+    dispatch_async(dispatch_get_main_queue(), ^{
+        AppDelegate *delegate = (AppDelegate *)[UIApplication sharedApplication].delegate;
+        InteractableClient *client = delegate.interactableClient;
+        
+        client.endEvent = ^{
+            [self sendEventWithName:@"engagement_end" body:@{}];
+        };
+    });
+}
+
 
 RCT_EXPORT_METHOD(sendMessage:(NSString *)messageText callback:(RCTResponseSenderBlock)callback) {
     [Salemove.sharedInstance sendWithMessage:messageText completion:^(Message * _Nullable message, SalemoveError * _Nullable error) {
